89788c7c92dc40ea6cf8db0debbd9a4be7388698
basAI2018 <38459002+basAI2018@users.noreply.github.com>
2019-01-08 14:32:04 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -147,3 +147,5 @@ class WebFrontend
 header('Content-Type: text/html; charset=UTF-8');
 $frontend = new WebFrontend('knowledgebases/planes.xml');
 $frontend->main();
+$frontend = new WebFrontend('knowledgebases/planes.xml');
+$frontend->main();
